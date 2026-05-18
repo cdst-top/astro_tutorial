c9d6382f14ff040f0cf215f5895ad59db0118cdc
openxw <openxw@gmail.com>
2026-05-18 19:18:41 +0800

    完成辅导课

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://openxw.netlify.app/"
+  site: "https://openxw.netlify.app/",
+  integrations: [preact()]
 });
